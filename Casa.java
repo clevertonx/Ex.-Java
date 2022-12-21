0e10f1c75ba7961a80d52a31397e1de6b3913b0b
clevertonx <clevertonx@gmail.com>
2022-12-21 09:27:35 -0400

    att

--- Casa.java
+++ Casa.java
@@ -2,23 +2,49 @@
 abra-as e feche-as como desejar. Utilize o método quantasPortasEstaoAbertas
 para imprimir o número de portas abertas. */
 public class Casa {
-    String corA, corB, corC;
-    boolean aberta;
-    String porta1, porta2, porta3;
+    String cor;
+    boolean porta1;
+    boolean porta2;
+    boolean porta3;
 
-    void pintar(String corA){
-        String corDaPorta1 = this.corA = corA;
-        System.out.println("A cor da porta1 é:" +corDaPorta1);
+    void pinta(String cor) {
+        System.out.println("A cor da casa é: " + cor);
     }
 
-    void pintar(String corB){
-        String corDaPorta2 = this.corB = corB;
-        System.out.println("A cor da porta1 é:" +corDaPorta2);
+    void abre() {
+        System.out.println("porta 1 aberta? " + this.porta1);
+        System.out.println("porta 2 aberta? " + this.porta2);
+        System.out.println("porta 3 aberta? " + this.porta3);
     }
 
-    void pintar(String corC){
-        String corDaPorta3 = this.corC = corC;
-        System.out.println("A cor da porta1 é:" +corDaPorta3);
+    int quantasAbertas() {
+
+        int portasAbertas = 0;
+
+        if (this.porta1) {
+            portasAbertas++;
+        }
+        if (this.porta2) {
+            portasAbertas++;
+        }
+        if (this.porta3) {
+            portasAbertas++;
+        }
+        return portasAbertas;
     }
-    
+
+    class TesteCasa {
+        public static void main(String[] args) {
+            Casa c1 = new Casa();
+            c1.pinta("black");
+            c1.porta1 = true;
+            c1.porta2 = false;
+            c1.porta3 = true;
+            c1.abre();
+            c1.quantasAbertas();
+
+            System.out.println("numero de portas abertas sao: " + c1.quantasAbertas());
+
+        }
+    }
 }
--- Porta.java
+++ Porta.java
@@ -1,49 +1,51 @@
-import javax.sound.sampled.Port;
+
 /*Crie uma porta, abra-a e feche-a. Pinte-a de diversas cores, altere suas dimensões e use o método estaAberta para verificar se ela está aberta. */
 public class Porta {
     String cor;
     boolean aberta;
     double dimensaox, dimensaoy, dimensaoz;
 
-    void pintar(String cor){
-        String corDaPorta = this.cor=cor;
-        System.out.println("A cor da porta é:" +corDaPorta);
+    void pintar(String cor) {
+        String corDaPorta = this.cor = cor;
+        System.out.println("A cor da porta é:" + corDaPorta);
     }
 
-    void abre(){
-        if (this.aberta == false){
+    void abre() {
+        if (this.aberta == false) {
             this.aberta = true;
         }
     }
 
-    void fecha(){
-        if (this.aberta == true){
+    void fecha() {
+        if (this.aberta == true) {
             this.aberta = false;
         }
     }
 
-    void estaAberta(){
-        if(this.aberta==true)
-        System.out.println("A porta está aberta.");
-        if (this.aberta==false)
-        System.out.println("A porta está fechada.");
+    void estaAberta() {
+        if (this.aberta == true)
+            System.out.println("A porta está aberta.");
+        if (this.aberta == false)
+            System.out.println("A porta está fechada.");
     }
 
-    public static void main (String args[]){
-        Porta porta = new Porta(); 
+    public static void main(String args[]) {
+        Porta porta = new Porta();
         porta.pintar("purple");
-        porta.dimensaox=1.20;
-        porta.dimensaoy=1.50;
-        porta.dimensaoz=0.50;
+        porta.dimensaox = 1.20;
+        porta.dimensaoy = 1.50;
+        porta.dimensaoz = 0.50;
         porta.abre();
         porta.estaAberta();
         porta.fecha();
         porta.estaAberta();
 
-        System.out.println("A cor da porta é: "+ porta.cor);
-        System.out.println("As dimensões da porta são:"+"\n"+"X: " + porta.dimensaox +"\n"+ "Y: " + porta.dimensaoy +"\n"+ "Z: " + porta.dimensaoz);
+        System.out.println("A cor da porta é: " + porta.cor);
+        System.out.println("As dimensões da porta são:" + "\n" + "X: " + porta.dimensaox + "\n" + "Y: "
+                + porta.dimensaoy + "\n" + "Z: " + porta.dimensaoz);
     }
 
-
+    public void mostrar() {
+    }
 
 }
